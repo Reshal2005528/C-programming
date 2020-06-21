4a3a26e405721364517e6fbb3174420212c36129
Reshal2005528 <67184872+Reshal2005528@users.noreply.github.com>
2020-06-21 12:35:56 +0530

    Create prime_number?.c

---
+++ prime_number?.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num,i,count;
+printf("enter the number:\t");
+scanf("%d",&num);
+for(i=2;i<num/2;i++)
+{
+if(num%i==0){
+    count++;}
+}
+if(count>0){printf("it is a prime number");}
+else
+printf("not a prime number");
+}
