5829b9d0df75b4b0a4a1afab083833776f1eee7b
Reshal2005528 <67184872+Reshal2005528@users.noreply.github.com>
2020-06-21 12:19:02 +0530

    Create matrix_addition.c

---
+++ matrix_addition.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+int A[10][10],B[10][10],C[10][10];
+int i,j,Arow,Brow,Crow,Acol,Bcol,Ccol;
+
+printf("Enter the rows ad columns of matrix A\n ");
+scanf("%d%d",&Arow,&Acol);
+printf("Matrix A:\n");
+for(i=0;i<Arow;i++)
+for(j=0;j<Acol;j++)
+scanf("%d",&A[i][j]);
+
+printf("Enter the rows ad columns of matrix B \n");
+scanf("%d%d",&Brow,&Bcol);
+printf("Matrix B:\n");
+for(i=0;i<Brow;i++)
+for(j=0;j<Bcol;j++)
+scanf("%d",&B[i][j]);
+
+if(Arow==Brow && Acol==Bcol)
+{
+printf("Matrix C :\n");
+for(i=0;i<Arow;i++)
+{
+for(j=0;j<Acol;j++)
+{
+C[i][j]=A[i][j]+B[i][j];
+printf("%d\t",C[i][j]);
+}
+printf("\n");
+}
+}
+else
+    printf("Matrix addition not possible");
+}
