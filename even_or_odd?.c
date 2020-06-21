867a0b5a45b39f1a3f59fcec3da62dec6d8c13d8
Reshal2005528 <67184872+Reshal2005528@users.noreply.github.com>
2020-06-21 12:33:22 +0530

    Create even_or_odd?.c

---
+++ even_or_odd?.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter a number\n");
+scanf("%d",&n);
+if(n%2==0)
+printf("entered numner is a even number");
+else
+printf("entered number is a odd number");
+}
