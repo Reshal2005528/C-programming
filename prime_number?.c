217f6a60663a153eaa092340b5fff9f4c993b9a0
Reshal2005528 <67184872+Reshal2005528@users.noreply.github.com>
2020-06-30 21:27:42 +0530

    Update prime_number?.c

--- prime_number?.c
+++ prime_number?.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 void main()
 {
-int num,i,count;
+int num,i,count=0;
 printf("enter the number:\t");
 scanf("%d",&num);
 for(i=2;i<num/2;i++)
 {
-if(num%i==0){
-    count++;}
+if(num%i==0)
+{
+    count++;
 }
-if(count>0){printf("it is a prime number");}
+}
+if(count>0)
+printf("it is a prime number");
 else
 printf("not a prime number");
 }
